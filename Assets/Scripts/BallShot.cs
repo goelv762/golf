dab430be36facc5acfa6bf7cd3b4db3f7e040fa0
Varun Goel <89370605+Toa-st@users.noreply.github.com>
2023-07-29 17:43:31 +1200

    comments 👍

--- Assets/Scripts/BallShot.cs
+++ Assets/Scripts/BallShot.cs
@@ -30,6 +30,9 @@ public class BallShot : MonoBehaviour
     // stores weather there is a shot currently active (e.g. pulling back to shoot)
     private bool isShotActive;
 
+    // stores weather a indicator is active
+    private bool isIndicatorActive;
+
     // stores shot length for speed
     private float shotVectorLength;
 
@@ -53,13 +56,23 @@ public class BallShot : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        // for hitting the ball
         BallHit();
+
+        // for changing the colour of ball
         BallColour();
+
+        // showing and hiding the shot indicator
         ManageIndicator();
+
+        // gets the normalised relitave vector of the shot
         shotVector = GetShotVector();
     }
 
+    // when mouse is over ball
     private void OnMouseEnter() { isOverBall = true; }
+
+    // when mouse moved away from ball / not on ball
     private void OnMouseExit() { isOverBall = false; }
 
     private void BallHit() {
@@ -69,30 +82,37 @@ public class BallShot : MonoBehaviour
             isShotActive = true; 
         }
 
-        // release
+        // if the mouse HAS been released and is not over ball --> shoot
         else if (BallMaster.shotActivate == 0f && !isOverBall && isShotActive) { 
             isShotActive = false;
 
+            // if shot power exeeds maxPower
             if (shotVectorLength > maxPower) {
                 ballRB.AddForce(-shotVector * maxPower * speedMulti, ForceMode2D.Impulse);
             } 
             
+            // if it doesn't
             else {
                 ballRB.AddForce(-shotVector * shotVectorLength * speedMulti, ForceMode2D.Impulse);
             }
 
+        // if the mouse HAS been released but is over ball --> cancel shot
         } else if (BallMaster.shotActivate == 0f && isOverBall && isShotActive) {
             isShotActive = false;
         }
     }
     
     private void ManageIndicator() {
-
+        // if there is a shot active and not too small of a shot
         if (isShotActive && (shotVectorLength > minIndicatorDistance)) {
+
+            // how many indicators need to be shown
             int targetIndicators = Mathf.RoundToInt(Mathf.Ceil(shotVectorLength));
 
+            // loops through all indicators
             for (int k = 0; k < arrowIndicators.Length; k++) {
                 // hides or shows the indicators based of shotVectorLength
+                // if it needs to be shown (< target) --> update position, set to visible
                 if (k < targetIndicators) {
                     Vector3 relativeIndicatorPos = Vector3.zero;
                     Vector2 currShotVector = shotVector * (k + 1) * arrowIndicatorSpacing;
@@ -101,27 +121,45 @@ public class BallShot : MonoBehaviour
 
                     // use negitave to get other side of ball 
                     arrowIndicators[k].transform.position = -relativeIndicatorPos + transform.position;
+
                     // use negitave to face opposite direction of ball
                     arrowIndicators[k].transform.up = -shotVector;
-                    arrowIndicators[k].SetActive(true); 
+
+                    // sets to visible (active)
+                    arrowIndicators[k].SetActive(true);
+
+                    // sets bool to true to make sure list is not being looped through for no reason
+                    isIndicatorActive = true;
                 }
+                // if it does not need to be shown (>= target) --> don't update position, hide it
                 else if (k >= targetIndicators) { 
+                    // sets to invisible (inactive)
                     arrowIndicators[k].SetActive(false); 
+
+                    // sets bool to false to make sure list is not being looped through for no reason
+                    isIndicatorActive = false;
                 } 
             }
         } 
-        
-        else {
+
+        // checks if there is not an active shot and the indicator is still showing
+        else if (!isShotActive && isIndicatorActive) {
+
+            // loops through all indicators
             foreach (GameObject indicator in arrowIndicators) {
-            indicator.SetActive(false);
+
+                // sets to invisible (inactive)
+                indicator.SetActive(false);
             }
         }
     }
 
     private void BallColour() {
+        // if there is an active shot or the mouse is over the ball --> change colour
         if (isShotActive || isOverBall) {
             ballRenderer.material.color = hoverColour;
         } 
+        // otherwise, set to default
         else {
             ballRenderer.material.color = defaultColour;
         }
@@ -130,15 +168,16 @@ public class BallShot : MonoBehaviour
     private Vector2 GetShotVector() {
         Vector2 currShotVector;
         // get vector from ball pos to mouse pos
+        // relative vector freom ball to mouse (shotPos) 
         currShotVector.x = BallMaster.shotPos.x - transform.position.x;
         currShotVector.y = BallMaster.shotPos.y - transform.position.y;
 
-        // pythagoras theorem (finding hypotenuse of triangle)
+        // pythagoras theorem (finding hypotenuse of triangle), used to normalise vector
         // c = √(a^2 + b^2)
         // a = x, b = y
         shotVectorLength = Mathf.Sqrt(Mathf.Pow(currShotVector.x, 2) + Mathf.Pow(currShotVector.y, 2));
 
-        // normalise the vector
+        // return the normalised the vector by dividing by length
         return currShotVector / shotVectorLength;
     }
 }
