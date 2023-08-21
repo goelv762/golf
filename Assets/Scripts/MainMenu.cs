d0795d0f23b37b9678463b929770dcd13957bf8e
Varun Goel <goelv@newlands.school.nz>
2023-08-21 18:45:08 +1200

    trialling components

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
+
+    [Header("Trialling components")]
+    [SerializeField] private RectTransform button1;
+    [SerializeField] private RectTransform button2;
+
+    [SerializeField] private Slider x_pos;
+    [SerializeField] private Slider y_pos;
+    [SerializeField] private Slider scale;
+
     public void LoadGame() {
         // next index of scenes in build settings
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,5 +23,13 @@ public class MainMenu : MonoBehaviour
 
         // for built game
         Application.Quit();
+    }
+
+    private void Update() {
+        button1.position = new Vector3(x_pos.value + 540f + 18f, y_pos.value, 1f);
+        button1.localScale = Vector3.one * scale.value;
+
+        button2.position = new Vector3(-x_pos.value + 540f + 18f, y_pos.value, 1f);
+        button2.localScale = Vector3.one * scale.value;
     }
 }
