7b07892877fa09f448b01527d224f5efbebe95ee
Varun Goel <goelv@newlands.school.nz>
2023-08-24 12:46:52 +1200

    removed trialling components

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,14 +4,6 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
-    [Header("Trialling components")]
-    [SerializeField] private RectTransform button1;
-    [SerializeField] private RectTransform button2;
-
-    [SerializeField] private Slider x_pos;
-    [SerializeField] private Slider y_pos;
-    [SerializeField] private Slider scale;
-
     public void LoadGame() {
         // next index of scenes in build settings
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -23,17 +15,5 @@ public class MainMenu : MonoBehaviour {
 
         // for built game
         Application.Quit();
-    }
-
-    private void Update() {
-        button1.localPosition = new Vector3(-x_pos.value, y_pos.value, 1f);
-        button1.localScale = Vector3.one * scale.value;
-
-        button2.localPosition = new Vector3(x_pos.value, y_pos.value, 1f);
-        button2.localScale = Vector3.one * scale.value;
-
-        Debug.Log("x - pos: " + button1.localPosition.x + 
-                  " | y - pos: " + button1.localPosition.y +
-                  " | scale: " + button1.localScale.x);
     }
 }
