097386b3c881db0cf5c4e4a5c3863b7ee2c58200
Varun Goel <89370605+Toa-st@users.noreply.github.com>
2023-08-27 11:11:05 +1200

    can build with editor quit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,8 +10,9 @@ public class MainMenu : MonoBehaviour {
 
     public void QuitGame() {
         // for editor
+# if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-
+# endif
         // for built game
         Application.Quit();
     }
