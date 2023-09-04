36e3c614af9ec2872cc640859eb731966ee5d979
Varun Goel <goelv@newlands.school.nz>
2023-09-05 09:33:35 +1200

    basic pause menu pannel functional

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour {
-    // used to turn down brightness of bg
-    [SerializeField] private Camera mainCamera;
     // canvas containing pause menu options
     [SerializeField] private Canvas pauseMenuCanvas;
+    // used to make bg dark
+    [SerializeField] private Canvas darkenCanvas;
 
     public void OpenPauseMenu() {
         
