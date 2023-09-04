e7fba3c54d9580733983cb1a73e6d8804190cfcd
Varun Goel <89370605+Toa-st@users.noreply.github.com>
2023-09-04 19:42:56 +1200

    basic pause manu canvas

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    // used to turn down brightness of bg
+    [SerializeField] private Camera mainCamera;
+    // canvas containing pause menu options
+    [SerializeField] private Canvas pauseMenuCanvas;
+
+    public void OpenPauseMenu() {
+        
+    }
+}
