273dbba708d5f829afbf7f79e19c7e5b0ad8ef8b
Varun Goel <goelv@newlands.school.nz>
2023-09-08 09:23:24 +1200

    VFX buttons visuals

--- Assets/Scripts/UI/VFX.cs
+++ Assets/Scripts/UI/VFX.cs
@@ -1,13 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class VFX : MonoBehaviour
 {
+    // text to be changed colour
+    [SerializeField] private TextMeshProUGUI VFXtext;
+
+    // colours to change to
+    [SerializeField] private Color VFXon;
+    [SerializeField] private Color VFXoff;
+
     [SerializeField] private GameObject[] VFXobjs;
     // true --> has VFX on
     // false --> has VFX off
     private bool VFXstate = true;
+
+    private void Start() {
+        VFXtext.color = VFXon;
+    }
 
     public void ToggleVFX() {
         // inverts VFX state when function is called
@@ -19,6 +29,12 @@ public class VFX : MonoBehaviour
         foreach (GameObject VFXobj in VFXobjs) {
             // stops particle systems
             VFXobj.SetActive(VFXstate);
-        }        
+        }
+
+        if (VFXstate) {
+            VFXtext.color = VFXon;
+        }   else {
+            VFXtext.color = VFXoff;
+        }
     }
 }
